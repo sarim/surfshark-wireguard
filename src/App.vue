33864535f2bbe9f9e9ba6657ae2d1b62bb56e295
Sarim Khan <sarim2005@gmail.com>
2022-06-11 19:28:50 +0600

    vuestic layout WIP

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,72 @@
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <va-navbar color="dark" shape class="mb-2">
+      <template #left>
+        <va-navbar-item>
+          <img
+            alt="Vue logo"
+            class="logo"
+            src="./assets/logo.svg"
+            width="125"
+            height="125"
+          />
+        </va-navbar-item>
+      </template>
+      <template #center>
+        <va-navbar-item>Dark</va-navbar-item>
+      </template>
+      <template #right>
+        <va-navbar-item>Right</va-navbar-item>
+      </template>
+    </va-navbar>
   </header>
-
+  <div class="content">Content</div>
+  <va-sidebar color="dark" minimizedWidth="64px" width="18rem">
+    <va-sidebar-item active-color="background">
+      <va-sidebar-item-content>
+        <va-sidebar-item-title>
+          background dark and auto text color
+        </va-sidebar-item-title>
+      </va-sidebar-item-content>
+    </va-sidebar-item>
+    <va-sidebar-item
+      v-for="item in items"
+      :key="item.to"
+      :active="item.active"
+      active-color="background"
+    >
+      <va-sidebar-item-content>
+        <va-icon :name="item.icon" />
+        <va-sidebar-item-title style="height: 24px">
+          {{ item.title }}
+        </va-sidebar-item-title>
+      </va-sidebar-item-content>
+    </va-sidebar-item>
+  </va-sidebar>
   <main>
+    <HelloWorld msg="You did it!" />
   </main>
 </template>
 
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
+
+const items = [
+  { title: "Dashboard", icon: "dashboard" },
+  { title: "Sidebar demo", icon: "room", active: true },
+  { title: "Loop", icon: "loop" },
+];
 </script>
+
+<style lang="scss">
+#app {
+  font-family: "Source Sans Pro", Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+body {
+  margin: 0;
+  background: var(--va-background);
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import { VuesticPluginsWithoutComponents } from 'vuestic-ui'
-import 'vuestic-ui/dist/styles/essential.css'
-import 'vuestic-ui/dist/styles/grid/grid.scss'
-import 'vuestic-ui/dist/styles/global/normalize.scss'
-import 'vuestic-ui/dist/styles/global/typography.scss'
+import { createVuestic } from "vuestic-ui";
+import "vuestic-ui/dist/vuestic-ui.css";
 
-createApp(App).use(VuesticPluginsWithoutComponents).mount("#app");
+createApp(App).use(createVuestic()).mount("#app");
